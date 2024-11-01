4ad6da262843c6337422b28f5ea9bde6b81e0cff
iwen-sue <grace2268@hotmail.com>
2024-10-31 20:14:40 -0700

    remove console log

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
     const token = req.cookies.token;
-    console.log("Token:" + token);
+    // console.log("Token:" + token);
     if (!token) return res.status(401).json({ message: 'Access denied' });
 
     try {
