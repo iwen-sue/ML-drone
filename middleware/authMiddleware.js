cdc74e961b6d0f37f7e5f5b6065250a26f0baab3
iwen-sue <grace2268@hotmail.com>
2024-11-22 13:35:14 -0800

    rollback

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken');
 
 
 const authenticateToken = (req, res, next) => {
-    // const authHeader = req.headers['authorization'];
-    // const token = authHeader && authHeader.split(' ')[1];
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
-    const token = req.cookies.token;
-    
+    // const token = req.cookies.token;
+
     console.log("Token:" + token);
     
     if (!token) return res.status(401).json({ message: 'Access denied' });
