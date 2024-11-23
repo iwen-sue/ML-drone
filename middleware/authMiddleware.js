70a7b56b6084646fae5404efaa61754210b56799
iwen-sue <grace2268@hotmail.com>
2024-11-22 22:10:56 -0800

    finally

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken');
 
 
 const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    // const authHeader = req.headers['authorization'];
+    // const token = authHeader && authHeader.split(' ')[1];
 
-    const cookieToken = req.cookies.token;
-    console.log("Cookie Token:" + cookieToken);
+    const token = req.cookies.token;
+    console.log("Cookie Token:" + token);
     
     if (!token) return res.status(401).json({ message: 'Access denied' });
 
