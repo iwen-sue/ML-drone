8e1768e2cde1f5862f5061040c35851816d32ea2
iwen-sue <grace2268@hotmail.com>
2024-11-22 18:18:15 -0800

    add image generator functionality

--- controllers/MLController.js
+++ controllers/MLController.js
@@ -3,6 +3,7 @@ const { updateEndpoint } = require("./endpointController");
 
 const generateImage = async (req, res) => {
   const email = req.user.email;
+  const text = req.body.text;
   
   try {
     await updateEndpoint(req);
@@ -13,9 +14,18 @@ const generateImage = async (req, res) => {
       return res.status(400).json({ message: "Request limit exceeded" });
     }
     await user.save();
+    
+    console.log("Generating image...");
+    const { Client } = await import('@gradio/client');
 
-    // Make image generation api call here
-    res.send( { message: "Image generated successfully", user });
+    const client = await Client.connect("grace2268/black-forest-labs-FLUX.1-dev");
+    const result = await client.predict("/predict", { 		
+        param_0: text? text : "Hello, World!"
+    });
+
+    console.log(result.data);
+
+    res.status(200).json({ message: "Image generated successfully", result: result.data });
 
   } catch (error) {
     res.status(500).json({ message: "Server error: ", error });
